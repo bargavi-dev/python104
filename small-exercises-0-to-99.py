0d2e821aa96a75ce58f270a81003222884af86d9
Bargavi  Kanneganti <bargavikanneganti@Bargavis-MBP.attlocal.net>
2020-08-20 11:20:56 -0500

    added small exercises to python104

---
+++ small-exercises-0-to-99.py
@@ -0,0 +1,10 @@
+for number in range(100):
+    print(number)
+
+for number in range(100):
+    if number % 2 == 0:
+        print(number)
+
+for number in range(100):
+    if number % 2 != 0:
+        print(number)
